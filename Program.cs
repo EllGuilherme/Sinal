e3ed1359f2604cf7ee7554635eedd7ad09421c1a
EllGuilherme <grds1000@gmail.com>
2022-04-01 22:36:13 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int NumeroDigitado; 
+ Console.Write("Digite um numero: ");
+ NumeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+  if  (NumeroDigitado <0 )
+   {
+      Console.ForegroundColor = ConsoleColor.DarkRed;
+      Console.WriteLine(" Esse número é negativo");
+   }
+
+  else if  (NumeroDigitado >0 )
+   {
+      Console.ForegroundColor = ConsoleColor.DarkBlue;
+      Console.WriteLine(" Esse número é positivo");
+   }
+  
+  else
+   {
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine(" Esse número é igual a Zero");
+   }
+      Console.ResetColor();
